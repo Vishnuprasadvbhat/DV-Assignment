980ebda717d09877c6c4b1490c6b4e44b9359563
wargod <vishnuprasadvbhat@gmail.com>
2025-02-11 10:19:47 +0530

    feat: update api

--- todoapp_django/taskapp/views.py
+++ todoapp_django/taskapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
 
 # Home Page - Display all tasks
-def index(request):
+def home(request):
     tasks = Task.objects.all()
     return render(request, 'content/home.html', {'tasks': tasks})
 
@@ -15,28 +15,32 @@ def create_task(request):
         if title and description:
             tasks =Task.objects.create(title=title, description=description, completed=False)
             tasks.save()
-            return redirect('index')
+            return redirect('home')
 
     return render(request, 'content/create_task.html')
 
 # Update Task Page
-def update_task(request, task):
-    task = get_object_or_404(Task, id=task.id)
+def update_task(request, id):
+
+    task = get_object_or_404(Task, id=id)
 
     if request.method == 'POST':
-        task.completed = not task.completed  # Toggle status
-        task.save()
-        return redirect('index')
+        title = request.POST.get('title') 
+        description = request.POST.get('description')
+        if title and description:
+            task= Task.objects.update(id=id, title=title,description=description, completed = True)
+            task.save()
+        return redirect('home')
 
     return render(request, 'content/update_task.html', {'task': task})
 
 
 # Delete Task Page
-def delete_task(request, task_id):
-    task = get_object_or_404(Task, id=task_id)
+def delete_task(request, id):
+    task = get_object_or_404(Task, id=id)
 
     if request.method == 'POST':
         task.delete()
-        return redirect('index')
+        return redirect('home')
 
     return render(request, 'content/delete_task.html', {'task': task})
