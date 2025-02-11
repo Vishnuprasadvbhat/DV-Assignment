f4fb17ca754efcee540e7a534a6066315de87543
wargod <vishnuprasadvbhat@gmail.com>
2025-02-11 10:44:03 +0530

    feat: error handling

---
+++ README.md
@@ -0,0 +1,24 @@
+# Assignment for Developer Role
+
+## List of Assigments:
+
+### Task 1
+#### Write a python program to find the unique words in the given sentence
+##### Solution given in the folder "coding_questions" as "unique_words.py"
+
+### Task 2
+#### Rewrite the function to reduce time complexity.
+##### Solution given in the folder "coding_questions" as "duplicates.py"
+
+---
+
+### Task 3
+#### Build a Simple API Using Flask (or Django, if preferred)
+##### Requirements:
+Use Flask (or Django/FastAPI) to create an API with the following endpoints:
+1. POST /tasks → Add a new task 
+2. GET /tasks → Get all tasks
+3. PUT /tasks/<task_id> → Update a task
+4. DELETE /tasks/<task_id> → Delete a task
+5. Store tasks in a list (or a database if comfortable).
+8. Ensure error handling (e.g., task not found, invalid input).
--- todoapp_django/taskapp/views.py
+++ todoapp_django/taskapp/views.py
@@ -1,43 +1,56 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
 
-# Home Page - Display all tasks
+# Display all tasks
 def home(request):
-    tasks = Task.objects.all()
-    return render(request, 'content/home.html', {'tasks': tasks})
+    try: 
+        tasks = Task.objects.all()
+        return render(request, 'content/home.html', {'tasks': tasks})
+    except:
+        print('Task not found')
 
 # Create Task Page
 def create_task(request):
-    if request.method == 'POST':
-        title = request.POST.get('title') 
-        description = request.POST.get('description')
+    try:
+        if request.method == 'POST':
+            title = request.POST.get('title') 
+            description = request.POST.get('description')
 
-        if title and description:
-            tasks =Task.objects.create(title=title, description=description, completed=False)
-            tasks.save()
-            return redirect('home')
+            if title and description:
+                tasks =Task.objects.create(title=title, description=description, completed=False)
+                tasks.save()
+                return redirect('home')
+    except Exception as e:
+        print(f"Encountered an errror {e}")
 
-    return render(request, 'content/create_task.html')
+    finally:
+        return render(request, 'content/create_task.html')
 
 # Update Task Page
 def update_task(request, id):
+    try: 
+        task = get_object_or_404(Task, id=id)
 
-    task = get_object_or_404(Task, id=id)
-
-    if request.method == 'POST':
-        task.completed = True
-        task.save()
-        return redirect('home')
-
-    return render(request, 'content/update_task.html', {'task': task})
+        if request.method == 'POST':
+            task.completed = True
+            task.save()
+            return redirect('home')
+    except Exception as e:
+        print(f'Updated faild: Due to Error {e}')
+    finally:
+        return render(request, 'content/update_task.html', {'task': task})
 
 
 # Delete Task Page
 def delete_task(request, id):
-    task = get_object_or_404(Task, id=id)
+    try:
+        task = get_object_or_404(Task, id=id)
 
-    if request.method == 'POST':
-        task.delete()
-        return redirect('home')
-
-    return render(request, 'content/delete_task.html', {'task': task})
+        if request.method == 'POST':
+            task.delete()
+            return redirect('home')
+    except Exception as e:
+        print(f'Error  {e} Occured, Refreshing...')
+    
+    finally:
+        return render(request, 'content/delete_task.html', {'task': task})
